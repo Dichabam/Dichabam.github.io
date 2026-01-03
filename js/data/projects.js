a8a43fb5a7a43293d3c6a5fdd0445d20dbdbae8f
Dichaba <mofokengdichaba@gmail.com>
2026-01-03 17:35:50 +0200

    refactor: update hero section and modify project data

--- js/data/projects.js
+++ js/data/projects.js
@@ -14,21 +14,5 @@ export const internalProjectData = [
     github: "https://github.com/Dichabam/Phumla-Kamnandi-GRP-12.git",
     preview: null,
   },
-  {
-    type: "project",
-    title: "My Website",
-    description: "this is my website",
-    icon: "./assets/images/dwpic.png",
-    github: "https://github.com/Dichabam/Dichaba-s_Website.git",
-    preview: null,
-  },
-  {
-    type: "project",
-    title: "EchoMaze",
-    description:
-      'EchoMaze is an immersive Android maze exploration game that combines motion controls with audio-visual feedback. Players navigate a procedurally generated maze using "sonar" where audio pitch and volume indicate proximity to walls and a visual raycasting interface.',
-    icon: "./assets/images/echo.png",
-    github: "https://github.com/Dichabam/EchoMaze.git",
-    preview: null,
-  },
+  
 ];
